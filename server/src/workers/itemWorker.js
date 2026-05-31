d84917a6f3d45b8b49d9a8046e414f06b65ccffc
2004vidya <pandey.vidya013@gmail.com>
2026-06-01 02:03:08 +0530

    item worker

--- server/src/workers/itemWorker.js
+++ server/src/workers/itemWorker.js
@@ -1,6 +1,9 @@
 import { Worker } from "bullmq";
 import connection from "../config/redis.js";
 import ItemModel from "../models/items.model.js";
+import extractContent from "../services/contentExtractor.js";
+import { generateTags } from "../services/ai.service.js";
+import { ingestContent } from "../services/ingestion.service.js";
 
 const itemWorker = new Worker("itemQueue", async (job) => {
     console.log("Processing job:", job.name, job.data);
@@ -9,9 +12,39 @@ const itemWorker = new Worker("itemQueue", async (job) => {
         const { itemId, url } = job.data;
         console.log(`Processing item ${itemId} from URL ${url}`);
 
+        const extractedData = await extractContent(url);
+
+        const tags = await generateTags(extractedData.content);
+        console.log(`Tags generated for item ${itemId}:`, tags);
+
+        
+
+        const result = await ingestContent({
+            title: extractedData.title,
+            url: extractedData.url,
+            content: extractedData.content,
+            tags: tags,
+        });
+
         // Update status to processed in DB
-        await ItemModel.findByIdAndUpdate(itemId, { status: "processed" });
+        await ItemModel.findByIdAndUpdate(itemId,
+            {
+                title: extractedData.title,
+                content: extractedData.content,
+                metadata: extractedData.metadata,
+                tags:tags,
+                status: "processed",
+                
+            }
+        );
         console.log(`Item ${itemId} marked as processed`);
+
+
+       
+
+        
+
+         
     }
 }, { connection });
 
